7327f3f4581efa7387b84f6b27cbd67b13642c02
Andrew <andrew.casin@gmail.com>
2020-11-04 23:15:26 -0600

    commit tree 47294668edfe210c106a3d740af3390ea334e9d1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name #initialize name
+        @roster = {} #initialize an empty hash roster
+    end
+
+    def add_student(student_name, grade) #study this method of creating new key and shovel to empty array
+        roster[grade] ||= [] #documentation on || usage?
+        roster[grade] << student_name
+    end
+
+    def grade(student_grade)
+        roster[student_grade] #return the values for the input student_grade key
+    end
+
+    def sort
+        sorted = {} #create empty hash for sorting
+        roster.each do |grade, students| #for each grade/students in the roster,
+            sorted[grade] = students.sort  #sort the students in each grade key of sorted
+        end
+        sorted #call sorted to return
+    end
+end
